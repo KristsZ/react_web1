65378177d9f8149fe87b691e6c82cf7af4fa639a
KristsZ <krists.zarans@gmail.com>
2025-06-04 19:55:01 +0300

    react_web1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,22 @@
+import { Carousel } from 'nuka-carousel';
+
+
+const img1 = 'https://placehold.co/600x150/EEE/31343C?font=roboto&text=Roboto';
+const img2 = 'https://placehold.co/600x150/EEE/31343C?font=lato&text=Lato';
+const img3 = 'https://placehold.co/600x150/EEE/31343C?font=lora&text=Lora';
+
+
 function Home() {
     return (
         <>
+            <Carousel showArrows="always" showDots="true" wrapMode="wrap">
+                <img src={img1} />
+                <img src={img2} />
+                <img src={img3} />
+            </Carousel>
             <h1>Galvenā</h1>
-            
+
+
         </>
 
     );
